fb25942c11f546807aad53ab4d72da08842a01a7
Nicolas <edward.nmontoya@gmail.com>
2021-02-06 23:25:59 -0500

    Added Container NoInfo component

---
+++ src/components/NoInfo/index.js
@@ -0,0 +1,16 @@
+import { Container } from "./styles"
+
+export const NoInfo = () => {
+
+  const emptyImg = "https://firebasestorage.googleapis.com/v0/b/eecowin-773e2.appspot.com/o/undraw_empty_xct9.svg?alt=media&token=055f8be4-903e-4125-88c2-a80e3123aeb7";
+
+  return (
+      <>
+        <Container>
+            <img src={emptyImg} width="400px" alt="empty image posts"></img>
+            <p>No hay información disponible</p>
+        </Container>
+      </>
+    ) 
+
+}
---
+++ src/components/NoInfo/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding:2%;
+  > p {
+    font-size: 40px;
+  }
+`;
